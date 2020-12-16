7d454ff4712ba04bdc45c2b61a0d9e26aabd7fc2
samarth767 <63947003+samarth767@users.noreply.github.com>
2020-12-16 21:08:09 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+import firebase from "firebase"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBSd9cITVuOxaBEYhSdw-xLIxtgp05cXw0",
+    authDomain: "bartersystem-4d8b5.firebaseapp.com",
+    projectId: "bartersystem-4d8b5",
+    storageBucket: "bartersystem-4d8b5.appspot.com",
+    messagingSenderId: "294047494534",
+    appId: "1:294047494534:web:1689099cc1b82e3d347c07"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  export default firebase.firestore();
